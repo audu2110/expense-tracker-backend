83b601ffe2426a5cd69f35f6356eb63b8b0bbb8a
Audumbar <audumbarak2000@gmail.com>
2023-02-16 14:12:24 +0530

    Third Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bcrypt = require('bcrypt')
 var app=express();
 var cors=require('cors');
 const sequelize = require('./util/database');
@@ -29,9 +30,14 @@ app.post('/post-signup',async (req,res,next)=>{
         var name=req.body.name;
         var email=req.body.email;
         var password=req.body.password;
-        const data= await User.create({name:name, email:email, password:password})
+        // const data= await User.create({name:name, email:email, password:password})
         // res.status(201).json({newExpenseDetail:data})
-        res.status(201).json({message: 'Successfuly create new user'})
+        const saltrounds = 10;
+        bcrypt.hash(password, saltrounds, async (err, hash) => {
+            console.log(err)
+            await User.create({ name, email, password: hash })
+            res.status(201).json({message: 'Successfuly create new user'})
+        })
     }
     catch(err){
         // res.sendFile(path.join(__dirname,'views','index.html'))
@@ -51,18 +57,20 @@ app.post('/post-login',async (req,res,next)=>{
         var email=req.body.email;
         var password=req.body.password;
         const user  = await User.findAll({ where : { email }})
-        console.log(user[0].email);
-        console.log(user[0].password);
+        // console.log(user[0].email);
+        // console.log(user[0].password);
         if(user.length>0){
-            if(user[0].email===email && user[0].password===password){
-                return res.status(200).json({success: true, message: "User logged in successfully"})
-            }
-            else if(user[0].password!=password){
-                return res.status(400).json({success: false, message: 'Password is incorrect'})
-            }
-            else{
-                return res.status(400).json({success: false, message: 'Something went wrong'})
-            }
+            bcrypt.compare(password, user[0].password, (err, result) => {
+                if(err){
+                 throw new Error('Something went wrong')
+                }
+                 if(result === true){
+                     return res.status(200).json({success: true, message: "User logged in successfully"})
+                 }
+                 else{
+                 return res.status(400).json({success: false, message: 'Password is incorrect'})
+                }
+             })
         }
         else{
             return res.status(404).json({success: false, message: 'User Doesnot exitst'})
